952dbc7ffe09607e4ed89b151f85d32d0dd7a278
ethanisthin <ethanalmeida184@gmail.com>
2025-09-04 18:21:09 -0600

    got multiple tasks running

--- src/main.c
+++ src/main.c
@@ -1,23 +1,50 @@
-#include "uart.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+// Task function prototypes
+void vTask1(void *pvParameters);
+void vTask2(void *pvParameters);
+void vTask3(void *pvParameters);
 
 int main(void) {
-    uart_init();
+    xTaskCreate(vTask1, "Task1", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vTask2, "Task2", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    xTaskCreate(vTask3, "Task3", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+    vTaskStartScheduler();
     
-    uart_puts("=== IoT Edge Device Simulator ===\r\n");
-    uart_puts("Phase 1: Basic System Operational\r\n");
-    
-    int counter = 0;
-    
-    while (1) {
-        uart_puts("Heartbeat: ");
-        uart_putc('0' + (counter % 10));
-        uart_puts("\r\n");
-        
-        counter++;
-        if (counter >= 10) {
-            counter = 0;
-        }
+    return -1;
+}
+
+void vTask1(void *pvParameters) {
+    for (;;) {
+        printf("Task 1 running\n");
+        vTaskDelay(pdMS_TO_TICKS(1000)); 
     }
-    
-    return 0;
+}
+
+void vTask2(void *pvParameters) {
+    for (;;) {
+        printf("Task 2 running\n");
+        vTaskDelay(pdMS_TO_TICKS(1500)); 
+    }
+}
+
+void vTask3(void *pvParameters) {
+    for (;;) {
+        printf("Task 3 running\n");
+        vTaskDelay(pdMS_TO_TICKS(2000)); 
+    }
+}
+
+void vApplicationMallocFailedHook(void) {
+    printf("Malloc failed!\n");
+    configASSERT(0);
+}
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName) {
+    printf("Stack overflow in task %s!\n", pcTaskName);
+    configASSERT(0);
 }
