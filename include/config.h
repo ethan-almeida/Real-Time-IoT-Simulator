e50fdd62f8653f8808b475219cf2753959782731
ethanisthin <ethanalmeida184@gmail.com>
2025-09-04 19:17:00 -0600

    added sensor simulations

--- include/config.h
+++ include/config.h
@@ -3,30 +3,22 @@
 
 #define SYS_NAME "Embedded Gateway"
 
-#define SENSOR_TASK_STACK_SIZE      (2048)
-#define PROCESSOR_TASK_STACK_SIZE   (4096)
+#define SENSOR_TASK_STACK_SIZE      (512)
+#define PROCESSOR_TASK_STACK_SIZE   (2048)
 #define NETWORK_TASK_STACK_SIZE     (8192)
 #define SECURITY_TASK_STACK_SIZE    (4096)
-#define MONITOR_TASK_STACK_SIZE     (2048)
-
-/* Queue Sizes */
+#define MONITOR_TASK_STACK_SIZE     (1024)
 #define SENSOR_QUEUE_LENGTH         (10)
 #define NETWORK_QUEUE_LENGTH        (5)
 #define MAX_MESSAGE_SIZE            (256)
-
-/* Network Configuration */
 #define MQTT_BROKER_ADDRESS         "test.mosquitto.org"
 #define MQTT_BROKER_PORT            8883
 #define MQTT_CLIENT_ID              "iot_gateway_sim_001"
 #define MQTT_TOPIC_BASE             "iot/gateway/"
-
-/* Sensor Configuration */
 #define NUM_TEMP_SENSORS            3
 #define NUM_HUMIDITY_SENSORS        2
 #define NUM_MOTION_SENSORS          1
 #define SENSOR_READ_INTERVAL_MS     1000
-
-/* Security Configuration */
 #define TLS_VERIFY_REQUIRED         1
 #define MAX_CERT_SIZE               4096
 
--- src/main.c
+++ src/main.c
@@ -1,101 +1,188 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdarg.h>
+#include <time.h>
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 #include "semphr.h"
+#include "event_groups.h"
+#include "config.h"
+#include "common.h"
+#include "tsk_priority.h"
 
-QueueHandle_t xQueue;
-SemaphoreHandle_t xResourceSemaphore;
+QueueHandle_t xSensorQueue = NULL;
+SemaphoreHandle_t xConsoleMutex = NULL;
+EventGroupHandle_t xSystemEvents = NULL;
 
-void vSenderTask(void *pvParameters);
-void vReceiverTask(void *pvParameters);
-void vResourceUserTask1(void *pvParameters);
-void vResourceUserTask2(void *pvParameters);
-void access_shared_resource(const char* taskName, int taskId);
+extern void vTemperatureSensorTask(void *pvParameters);
+extern void vHumiditySensorTask(void *pvParameters);
+extern void vMotionSensorTask(void *pvParameters);
+void vDataProcessorTask(void *pvParameters);
+void vSystemMonitorTask(void *pvParameters);
+void safe_printf(const char *format, ...);
+uint32_t get_system_time_ms(void);
 
 int main(void) {
-    xQueue = xQueueCreate(5, sizeof(uint32_t));
-    if (xQueue == NULL) {
-        printf("Error: Failed to create queue!\n");
+    printf("Starting IoT Gateway \n");
+    srand(time(NULL));
+
+    xSensorQueue = xQueueCreate(SENSOR_QUEUE_LENGTH, sizeof(sensor_data_t));
+    if (xSensorQueue == NULL) {
+        printf("Error: Failed to create sensor queue!\n");
         return -1;
     }
-
-    xResourceSemaphore = xSemaphoreCreateMutex();
-    if (xResourceSemaphore == NULL) {
-        printf("Error: Failed to create semaphore!\n");
+    
+    xConsoleMutex = xSemaphoreCreateMutex();
+    if (xConsoleMutex == NULL) {
+        printf("Error: Failed to create console mutex!\n");
         return -1;
     }
+    
+    xSystemEvents = xEventGroupCreate();
+    if (xSystemEvents == NULL) {
+        printf("Error: Failed to create event group!\n");
+        return -1;
+    }
+    
+    BaseType_t xReturned;
+    
+    for (int i = 0; i < NUM_TEMP_SENSORS; i++) {
+        char taskName[32];
+        snprintf(taskName, sizeof(taskName), "TempSensor%d", i);
+        
+        xReturned = xTaskCreate(
+            vTemperatureSensorTask,
+            taskName,
+            SENSOR_TASK_STACK_SIZE,
+            (void*)(intptr_t)i,
+            PRIORITY_SENSOR_LOW,
+            NULL
+        );
+        if (xReturned != pdPASS) {
+            printf("Error: Failed to create temperature sensor task %d\n", i);
+            return -1;
+        }
+    }
 
-    xTaskCreate(vSenderTask, "Sender", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-    xTaskCreate(vReceiverTask, "Receiver", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    xTaskCreate(vResourceUserTask1, "ResourceUser1", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
-    xTaskCreate(vResourceUserTask2, "ResourceUser2", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
-
+    for (int i = 0; i < NUM_HUMIDITY_SENSORS; i++) {
+        char taskName[32];
+        snprintf(taskName, sizeof(taskName), "HumidSensor%d", i);
+        
+        xReturned = xTaskCreate(
+            vHumiditySensorTask,
+            taskName,
+            SENSOR_TASK_STACK_SIZE,
+            (void*)(intptr_t)i,
+            PRIORITY_SENSOR_LOW,
+            NULL
+        );
+        if (xReturned != pdPASS) {
+            printf("Error: Failed to create humidity sensor task %d\n", i);
+            return -1;
+        }
+    }
+    
+    xReturned = xTaskCreate(
+        vMotionSensorTask,
+        "MotionSensor",
+        SENSOR_TASK_STACK_SIZE,
+        NULL,
+        PRIORITY_SENSOR_HIGH,
+        NULL
+    );
+    if (xReturned != pdPASS) {
+        printf("Error: Failed to create motion sensor task\n");
+        return -1;
+    }
+    
+    xReturned = xTaskCreate(
+        vDataProcessorTask,
+        "DataProcessor",
+        PROCESSOR_TASK_STACK_SIZE,
+        NULL,
+        PRIORITY_PROCESSOR,
+        NULL
+    );
+    if (xReturned != pdPASS) {
+        printf("Error: Failed to create data processor task\n");
+        return -1;
+    }
+    
+    xReturned = xTaskCreate(
+        vSystemMonitorTask,
+        "SystemMonitor",
+        MONITOR_TASK_STACK_SIZE,
+        NULL,
+        PRIORITY_MONITOR,
+        NULL
+    );
+    if (xReturned != pdPASS) {
+        printf("Error: Failed to create system monitor task\n");
+        return -1;
+    }
+    printf("Starting scheduler...\n");
     vTaskStartScheduler();
+    printf("Error: Scheduler returned!\n");
     return -1;
 }
 
-void vSenderTask(void *pvParameters) {
-    uint32_t ulValueToSend = 0;
+void vDataProcessorTask(void *pvParameters) {
+    sensor_data_t sensor_data;
+    
+    safe_printf("[DataProcessor] Started\n");
     
     for (;;) {
-        ulValueToSend++;
-        if (xQueueSend(xQueue, &ulValueToSend, pdMS_TO_TICKS(100)) != pdTRUE) {
-            access_shared_resource("Sender", 1);
-            printf("Sender: Failed to send value %u to queue\n", (unsigned int)ulValueToSend);
-        } else {
-            access_shared_resource("Sender", 1);
-            printf("Sender: Sent value %u to queue\n", (unsigned int)ulValueToSend);
+        if (xQueueReceive(xSensorQueue, &sensor_data, pdMS_TO_TICKS(1000)) == pdPASS) {
+            const char* sensor_type_str;
+            switch (sensor_data.type) {
+                case SENSOR_TYPE_TEMPERATURE:
+                    sensor_type_str = "Temperature";
+                    break;
+                case SENSOR_TYPE_HUMIDITY:
+                    sensor_type_str = "Humidity";
+                    break;
+                case SENSOR_TYPE_MOTION:
+                    sensor_type_str = "Motion";
+                    break;
+                default:
+                    sensor_type_str = "Unknown";
+                    break;
+            }
+            safe_printf("[DataProcessor] Processing %s sensor %d: %.2f\n",
+                       sensor_type_str, sensor_data.sensor_id, sensor_data.value);
+            vTaskDelay(pdMS_TO_TICKS(100));
         }
+    }
+}
+
+void vSystemMonitorTask(void *pvParameters) {
+    safe_printf("[SystemMonitor] Started\n");
+    for (;;) {
+        UBaseType_t uxSensorQueueMessages = uxQueueMessagesWaiting(xSensorQueue);
+        safe_printf("[SystemMonitor] Sensor queue has %lu messages\n", 
+                   (unsigned long)uxSensorQueueMessages);
         
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(5000));
     }
 }
 
-void vReceiverTask(void *pvParameters) {
-    uint32_t ulReceivedValue;
-    
-    for (;;) {
-        if (xQueueReceive(xQueue, &ulReceivedValue, pdMS_TO_TICKS(500)) == pdTRUE) {
-            access_shared_resource("Receiver", 2);
-            printf("Receiver: Received value %u from queue\n", (unsigned int)ulReceivedValue);
-        } else {
-            access_shared_resource("Receiver", 2);
-            printf("Receiver: No data available on queue\n");
-        }
-        vTaskDelay(pdMS_TO_TICKS(500));
+
+void safe_printf(const char *format, ...) {
+    va_list args;
+    va_start(args, format);
+    if (xSemaphoreTake(xConsoleMutex, portMAX_DELAY) == pdTRUE) {
+        vprintf(format, args);
+        fflush(stdout);
+        xSemaphoreGive(xConsoleMutex);
     }
+    va_end(args);
 }
 
-void vResourceUserTask1(void *pvParameters) {
-    for (;;) {
-        access_shared_resource("ResourceUser1", 3);
-        printf("ResourceUser1: Accessed shared resource\n");
-        vTaskDelay(pdMS_TO_TICKS(2000));
-        vTaskDelay(pdMS_TO_TICKS(3000));
-    }
+uint32_t get_system_time_ms(void) {
+    return xTaskGetTickCount() * portTICK_PERIOD_MS;
 }
-
-void vResourceUserTask2(void *pvParameters) {
-    for (;;) {
-        access_shared_resource("ResourceUser2", 4);
-        printf("ResourceUser2: Accessed shared resource\n");
-        vTaskDelay(pdMS_TO_TICKS(1500));
-        vTaskDelay(pdMS_TO_TICKS(4000));
-    }
-}
-
-void access_shared_resource(const char* taskName, int taskId) {
-    if (xSemaphoreTake(xResourceSemaphore, portMAX_DELAY) == pdTRUE) {
-        printf("[%s-%d] Entering critical section\n", taskName, taskId);
-        vTaskDelay(pdMS_TO_TICKS(100));
-        printf("[%s-%d] Leaving critical section\n", taskName, taskId);
-        xSemaphoreGive(xResourceSemaphore);
-    }
-}
-
 
 void vApplicationMallocFailedHook(void) {
     printf("Malloc failed!\n");
