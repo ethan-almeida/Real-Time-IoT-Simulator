dcbb83fee5f2642556f01964b3382ba1446029d0
ethanisthin <ethanalmeida184@gmail.com>
2025-09-04 19:17:13 -0600

    fixed to add sensors

--- include/FreeRTOSConfig.h
+++ include/FreeRTOSConfig.h
@@ -8,19 +8,14 @@
 #define configTICK_RATE_HZ                  ( ( TickType_t ) 100 )
 #define configMAX_PRIORITIES                ( 5 )
 #define configMINIMAL_STACK_SIZE            ( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE               ( ( size_t ) ( 17 * 1024 ) )
+#define configTOTAL_HEAP_SIZE               ( ( size_t ) ( 128 * 1024 ) )
 #define configMAX_TASK_NAME_LEN             ( 16 )
 #define configUSE_TRACE_FACILITY            0
 #define configUSE_16_BIT_TICKS              0
 #define configIDLE_SHOULD_YIELD             1
 #define configUSE_MUTEXES                   1
-
-/* Co-routine definitions. */
 #define configUSE_CO_ROUTINES               0
 #define configMAX_CO_ROUTINE_PRIORITIES     ( 2 )
-
-/* Set the following definitions to 1 to include the API function, or zero
-to exclude the API function. */
 #define INCLUDE_vTaskPrioritySet            1
 #define INCLUDE_uxTaskPriorityGet           1
 #define INCLUDE_vTaskDelete                 1
@@ -28,8 +23,6 @@ to exclude the API function. */
 #define INCLUDE_vTaskSuspend                1
 #define INCLUDE_vTaskDelayUntil             1
 #define INCLUDE_vTaskDelay                  1
-
-/* Normal assert() semantics without relying on the provision of an assert.h header file. */
 #define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
 
-#endif /* FREERTOS_CONFIG_H */
+#endif 
